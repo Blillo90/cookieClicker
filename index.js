47ae493a79ef320329f1d0f3fb447f85518336e0
Blillo90 <ppherrero90@gmail.com>
2024-12-07 10:27:59 +0100

    Anotaciones
    Añadido anotaciones en javascript después de haber entendido mejor el código

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+//Definimos la constante cookie para poder asociarle la función de hacer click más adelante
+
 const cookie = document.querySelector("#cookie")
+
+//Definimos las variables title y score para poder modificar el texto, tanto del título en la ventana de navegación como en el propio score en el centro de la pantalla
 
 const updateScore = cookies => {
     const title = document.querySelector("title")
@@ -7,13 +11,15 @@ const updateScore = cookies => {
     score.innerText = cookies;
     title.innerHTML = cookies + " cookies - Cookie Clicker"
 
-    localStorage.setItem("cookies", cookies);
+    localStorage.setItem("cookies", cookies); //Almacenamos en localStorage para que guarde las cookies incluso si se cierra el navegador
 }
 
 const getStorage = () => {
-    const cookies = localStorage.getItem("cookies") || 0;
-    const powerups = JSON.parse(localStorage.getItem("powerups")) || [];
+    const cookies = localStorage.getItem("cookies") || 0; //Obtiene el número de cookies del localStorage, y si no existe (es null) lo inicializa con 0
+    const powerups = JSON.parse(localStorage.getItem("powerups")) || []; //Obtiene el JSON (porque es un array y necesita JSON) de powerups y si no existe, crea un array vacío
 
+
+    //Almacenamos ambas variables, cookies y powerups, dentro de una única storage como dos cadenas de texto separadas por una coma
     const storage = {
         "cookies": cookies,
         "powerups": powerups
@@ -22,19 +28,22 @@ const getStorage = () => {
     return storage;
 }
 
+//Vamos a definir la función de hacer click en la galleta
+
 const cookieClicked = cookies => {
-    const storage = getStorage();
+    const storage = getStorage(); //Cogemos la cantidad que haya guardada en el navegador
 
-    const score = document.querySelector("#score span");
-    const scoreValue = cookies ? cookies : parseInt(score.innerText);
-
+    const score = document.querySelector("#score span"); //Le decimos que vaya al número de cookies del centro de la pantalla
+    const scoreValue = cookies ? cookies : parseInt(score.innerText); //la convertimos en un Int para poder sumar números
     let newScore;
 
-    newScore = scoreValue + 1;
+    newScore = scoreValue + 1; //newScore añade 1 al valor actual cada vez que se llama
 
-    updateScore(newScore);
+    updateScore(newScore); //Devuelve el valor de la función updateScore con el valor newScore
 }
 
+
+//Creamos la función para que ocurra algo cuando hacemos click
 cookie.addEventListener("click", () => {
-    cookieClicked()
+    cookieClicked() //En este caso, ocurre la función cookieClicked
 });
